1885802a0183c7c4ecd9493191c7fea413a15e94
makis674 <xrisiayginotiaslakonias@gmail.com>
2025-06-09 12:47:09 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -59,9 +59,12 @@ def handle_webhook():
     return "ok", 200
 
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
